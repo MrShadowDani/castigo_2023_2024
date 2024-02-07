a8a818f824456327b73114ba5bf6ffdfb0b5230a
MrShadowDani <dgonzsan@myuax.com>
2024-02-07 23:20:29 +0100

    guardar

--- Castigo.java
+++ Castigo.java
@@ -13,9 +13,41 @@ public class Castigo {
         System.out.println("El mayor número representable por un char es: " + Character.MAX_VALUE);
         System.out.println("El mayor número representable por un short es: " + Short.MAX_VALUE);
         System.out.println("El mayor número representable por un int es: " + Integer.MAX_VALUE);
+
+        // Parte 3: Calcular la suma de los múltiplos de 5 entre 1 y 100
+        int[] multiplosDe5 = encontrarMultiplosDe5(1, 100);
+        int suma = sumarArray(multiplosDe5);
+
+        System.out.println("La suma de los múltiplos de 5 entre 1 y 100 es: " + suma);
     }
 
     public static long calcularSegundosEnAnio(int dias, byte horas, byte minutos, byte segundos) {
         return (long) dias * horas * minutos * segundos;
+    }
+
+    public static int[] encontrarMultiplosDe5(int inicio, int fin) {
+        int cantidad = 0;
+        for (int i = inicio; i <= fin; i++) {
+            if (i % 5 == 0) {
+                cantidad++;
+            }
+        }
+
+        int[] multiplos = new int[cantidad];
+        int index = 0;
+        for (int i = inicio; i <= fin; i++) {
+            if (i % 5 == 0) {
+                multiplos[index++] = i;
+            }
+        }
+        return multiplos;
+    }
+
+    public static int sumarArray(int[] array) {
+        int suma = 0;
+        for (int num : array) {
+            suma += num;
+        }
+        return suma;
     }
 }
